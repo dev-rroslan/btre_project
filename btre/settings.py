d02932b2db00490004a470db52fae5b2380de840
dev-rroslan <user.email>
2023-12-01 14:44:11 +0800

    static root url

--- btre/settings.py
+++ btre/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles/'  # <-- Updated!
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # <-- Updated!
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR /  'btre/static']
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
